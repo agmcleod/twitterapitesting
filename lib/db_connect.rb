4f6c1cf54c95134a22ed16c1f32403b93ed5ef33
Aaron McLeod <sircoolguy99@gmail.com>
2013-05-07 19:03:58 -0400

    changed the db connection to use adapter based off of config

--- lib/db_connect.rb
+++ lib/db_connect.rb
@@ -6,7 +6,7 @@ class DbConnect
   def initialize
     self.config = YAML.load IO.read(File.expand_path(File.dirname(__FILE__) + '/../config/database.yml'))
     ActiveRecord::Base.establish_connection(
-      adapter: "postgresql",
+      adapter:  config['adapter'],
       database: config['database'],
       username: config['username'],
       password: config['password'],
