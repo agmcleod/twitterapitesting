c66505934bc9f3b7062020de0c84ed2277c97de1
Arthur Neves <arthurnn@gmail.com>
2013-05-07 20:31:20 -0400

    catch errors on client

--- lib/mention_track.rb
+++ lib/mention_track.rb
@@ -1,9 +1,18 @@
 class MentionTrack
   def initialize
     puts 'stream should start'
-    TweetStream::Client.new.track("@atweetdungeon") do |status|
+
+    client = TweetStream::Client.new
+
+    client.on_error do |message|
+      puts message
+    end
+
+    client.track("@atweetdungeon") do |status|
       puts "logged: #{status.text}"
-      Mention.create! text: status.text
+      Mention.create!(text: status.text, username: status.user.name)
+      puts Mention.all.to_a.inspect
+      puts status.text
     end
   end
 end
