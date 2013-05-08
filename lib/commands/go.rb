0a71263a6352f3dc61cf7dcd92808b6a79ec99ba
Aaron McLeod <sircoolguy99@gmail.com>
2013-05-07 21:49:39 -0400

    changes

--- lib/commands/go.rb
+++ lib/commands/go.rb
@@ -1,7 +1,6 @@
 class Go < Command
 
   def perform(*args)
-    @user.update_attribute(room_id: 1)
     
   end
 end
--- lib/models/room.rb
+++ lib/models/room.rb
@@ -1,3 +1,8 @@
 class Room < ActiveRecord::Base
   belongs_to :dungeon
+  has_many :exits
+
+  def exits
+    
+  end
 end
